2506e9f72130f0888c1ebae6d9b99d990078b2ec
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-02-26 07:23:49 +0900

    ABC049C,ABC085C modified

--- cmd/ABC049C/main.go
+++ cmd/ABC049C/main.go
@@ -1,52 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
+func reverse(s string) string {
+	rs := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return string(rs)
+}
+
+func substr(s string, i int, l int) string {
+	if i+l > len(s) {
+		return ""
+	}
+	return s[i : i+l]
+}
+
 func main() {
-	var s string
-	fmt.Scan(&s)
-	for {
-		if s = strings.Replace(s, "dreamereraser", "", 1); len(s) < 5 {
-			break
+	sc := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, 0)
+	sc.Buffer(buf, 131072)
+	sc.Scan()
+	s := sc.Text()
+	rs := reverse(s)
+	divide := []string{"maerd", "remaerd", "esare", "resare"}
+	var can = true
+	var can2 bool
+	i := 0
+	limit := len(s)
+	for i < limit {
+		can2 = false
+		for j := 0; j < 4; j++ {
+			d := divide[j]
+			if substr(rs, i, len(d)) == d {
+				can2 = true
+				i += len(d)
+			}
+			if i >= limit {
+				break
+			}
 		}
-		if s = strings.Replace(s, "dreamererase", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "dreameraser", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "dreamerase", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "eraseeraser", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "eraseerase", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "erasereraser", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "erasererase", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "dreamer", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "eraser", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "dream", "", 1); len(s) < 5 {
-			break
-		}
-		if s = strings.Replace(s, "erase", "", 1); len(s) < 5 {
+		if !can2 {
+			can = false
 			break
 		}
 	}
-	if len(s) == 0 {
+	if can {
 		fmt.Println("YES")
 	} else {
 		fmt.Println("NO")
--- cmd/ABC085C/main.go
+++ cmd/ABC085C/main.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	var n, t int
 	fmt.Scan(&n, &t)
-	now := time.Now()
 	for x := 0; x <= n; x++ {
 		for y := 0; y <= n-x; y++ {
-			for z := 0; z <= n-x-y; z++ {
-				if (10000*x+5000*y+1000*z == t) && (x+y+z == n) {
-					fmt.Println(x, y, z)
-					goto Z
-				}
+			z := n - x - y
+			total := 10000*x + 5000*y + 1000*z
+			if total == t {
+				fmt.Println(x, y, z)
+				goto Z
 			}
 		}
 	}
 	fmt.Println("-1 -1 -1")
 Z:
-	fmt.Printf("%vms\n", time.Since(now).Milliseconds())
 }
